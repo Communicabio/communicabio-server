9f308c7d5df7b77d85ed477115242aa2e940e7ae
aishutin <hazmozavr@gmail.com>
2020-08-10 10:05:00 +0300

    Auth fix

--- dialogs.py
+++ dialogs.py
@@ -6,6 +6,22 @@ import pymongo
 import requests
 import logging
 
+def request_auth(url):
+    """
+    https://cloud.google.com/run/docs/authenticating/service-to-service#console-ui
+    """
+    receiving_service_url = url
+    metadata_server_token_url = f'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
+    token_request_url = metadata_server_token_url + receiving_service_url
+    token_request_headers = {'Metadata-Flavor': 'Google'}
+
+    # Fetch the token
+    token_response = requests.get(token_request_url, headers=token_request_headers)
+    jwt = token_response.content.decode("utf-8")
+    # Provide the token in the request to the receiving service
+    receiving_service_headers = {'Authorization': f'bearer {jwt}'}
+    return receiving_service_headers
+
 class Manager:
     gpt2links = {
         'en': 'https://en-gpt2-ervice-pcdqvqhk7q-uc.a.run.app'
@@ -16,6 +32,7 @@ class Manager:
         self.db = self.client[f'communicabio-{lang}']
         with open(f'assets/dialog-{lang}.json') as file:
             self.dialogs = json.load(file)
+        self.gpt2_headers = request_auth(self.gpt2links[lang])
         #self.dialogs = self.db['dialogs-vectorized']
 
     def start(self) -> Phrase:
@@ -24,7 +41,8 @@ class Manager:
     def reply(self, history: List[Phrase]) -> Phrase:
         # ...
         resp = requests.post(self.gpt2links[self.lang],
-                    {'history': [el.text for el in history]})
+                    json={'history': [el.text for el in history]},
+                    headers=self.gpt2_headers)
         if resp.status_code != 200:
             logging.warning(resp.text)
             resp.raise_for_status()
